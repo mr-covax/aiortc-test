1819fcecf64214b8849c056c80fcc567d3491edf
mr-covax <180854280+mr-covax@users.noreply.github.com>
2024-11-27 17:43:28 +0300

    feat: switch to WebSockets for signalling

--- src/server.py
+++ src/server.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from aiortc import RTCPeerConnection, RTCSessionDescription
 from aiortc.contrib.media import MediaPlayer, MediaRecorder, MediaRelay
-
 
 class Descriptor(BaseModel):
     type: str
@@ -25,54 +24,63 @@ host_audio = None
 host_video = None
 relay: MediaRelay = MediaRelay()
 
+
+
 @app.get("/")
 async def get_code():
     with open("./static/index.html") as f:
         return HTMLResponse(f.read())
-
-@app.post("/offer", response_model=Descriptor)
-async def init_connection(desc: Descriptor):
+    
+@app.websocket("/join")
+async def join_room(ws: WebSocket):
     global host
+    await ws.accept()
     
-    peer = RTCPeerConnection()
-    if host is None:
-        host = peer
+    while True:
+        data = await ws.receive_json()
+        if data["type"] == "message":
+            print(data["message"])
+            await ws.send_json({"type": "message", "message": "I printed that shit!"})
 
-    offer = RTCSessionDescription(sdp=desc.sdp, type=desc.type)
+        elif data["type"] == "offer":
+            peer = RTCPeerConnection()
+            if host is None:
+                host = peer
 
-    @peer.on('track')
-    def on_track(track):
-        global host_audio, host_video
-        if track.kind == "audio":
-            if peer == host:
-                host_audio = track
-                peer.addTrack(relay.subscribe(host_audio))
-            else:
-                print("Fuck off")
-                peer.addTrack(relay.subscribe(host_audio))
+            offer = RTCSessionDescription(sdp=data["sdp"], type=data["type"])
+
+            @peer.on('track')
+            def on_track(track):
+                global host_audio, host_video
+                if track.kind == "audio":
+                    if peer == host:
+                        host_audio = track
+                        peer.addTrack(relay.subscribe(host_audio))
+                    else:
+                        print("Fuck off")
+                        peer.addTrack(relay.subscribe(host_audio))
+                    
+                if track.kind == "video":
+                    if peer == host:
+                        host_video = track
+                        peer.addTrack(relay.subscribe(host_video))
+                    else:
+                        peer.addTrack(relay.subscribe(host_video))
+
+                @track.on("ended")
+                async def on_ended():
+                    print("Track is ended :(")
+
+            @peer.on("connectionstatechange")
+            async def on_connectionstatechange():
+                print(f"The connection changed it state to: {peer.connectionState}")
             
-        if track.kind == "video":
-            if peer == host:
-                host_video = track
-                peer.addTrack(relay.subscribe(host_video))
-            else:
-                peer.addTrack(relay.subscribe(host_video))
+            await peer.setRemoteDescription(offer)
 
-        @track.on("ended")
-        async def on_ended():
-            print("Track is ended :(")
+            answer = await peer.createAnswer()
+            await peer.setLocalDescription(answer)
 
-    @peer.on("connectionstatechange")
-    async def on_connectionstatechange():
-        print(f"The connection changed it state to: {peer.connectionState}")
-    
-    await peer.setRemoteDescription(offer)
-
-    answer = await peer.createAnswer()
-    await peer.setLocalDescription(answer)
-
-    return Descriptor(
-        type=peer.localDescription.type,
-        sdp=peer.localDescription.sdp
-    )
-
+            await ws.send_json({
+                "type": peer.localDescription.type,
+                "sdp": peer.localDescription.sdp
+            })
